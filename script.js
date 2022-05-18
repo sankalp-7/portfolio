6d4d935317abcb1a210cfdcfade13c1f7c6096b6
sankalp <sankalpsodagum7@gmail.com>
2022-05-18 15:39:30 +0530

    changing the logic

--- script.js
+++ script.js
@@ -72,3 +72,32 @@ function classToggle()
      }
  });
 
+ function DownloadFile(fileName) {
+    //Set the File URL.
+    var url =fileName;
+
+    //Create XMLHTTP Request.
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        //Convert the Byte Data to BLOB object.
+        var blob = new Blob([req.response], { type: "application/octetstream" });
+
+        //Check the Browser type and download the File.
+        var isIE = false || !!document.documentMode;
+        if (isIE) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            var url = window.URL || window.webkitURL;
+            link = url.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.setAttribute("download", fileName);
+            a.setAttribute("href", link);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+    req.send();
+};
