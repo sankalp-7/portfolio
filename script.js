136079b95697927f76cac23c0578fabcbad787e6
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2024-02-28 13:28:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -74,30 +74,12 @@ function classToggle()
 
  function DownloadFile(fileName) {
     //Set the File URL.
-    var url =fileName;
-
-    //Create XMLHTTP Request.
-    var req = new XMLHttpRequest();
-    req.open("GET", url, true);
-    req.responseType = "blob";
-    req.onload = function () {
-        //Convert the Byte Data to BLOB object.
-        var blob = new Blob([req.response], { type: "application/octetstream" });
-
-        //Check the Browser type and download the File.
-        var isIE = false || !!document.documentMode;
-        if (isIE) {
-            window.navigator.msSaveBlob(blob, fileName);
-        } else {
-            var url = window.URL || window.webkitURL;
-            link = url.createObjectURL(blob);
-            var a = document.createElement("a");
-            a.setAttribute("download", fileName);
-            a.setAttribute("href", link);
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-        }
-    };
-    req.send();
+     var url="https://sweresume.s3.amazonaws.com/resume/sankalp's+resume.pdf"
+   var isIE = false || !!document.documentMode;
+    if (isIE) {
+        window.open(url, '_blank');
+    } else {
+        var win = window.open();
+        win.location.href = url;
+    }
 };
